19c1187f2bba693c0d836cf1c1f2d25bc17b7c47
iamavinashmourya <avinashgoweb@gmail.com>
2024-06-01 11:13:31 +0530

    Comments in Java

---
+++ src/Comments.java
@@ -0,0 +1,15 @@
+public class Comments {
+    /**
+     * this is main method - java docs comment
+     * @param args
+     */
+    public static void main(String[] args) {
+        // important line don't delete this - single line comment
+        System.out.println("hello world");
+        /*
+        this is first line
+        this is second line comment
+        Multi-Line Comment
+         */
+    }
+}
