228b66b4f73f2fe234baff39614fb8834ac2de10
iamavinashmourya <avinashgoweb@gmail.com>
2024-06-08 12:30:37 +0530

    This Keyword with example in Java

---
+++ src/ThisKeyword.java
@@ -0,0 +1,37 @@
+class myCar {
+    float currentFuelOfCar;
+    public myCar start(){
+        if (currentFuelOfCar==0){
+            System.out.println("Your not have fuel, pls refuel");
+        } else if (currentFuelOfCar<=5) {
+            System.out.println("Your car is started, but fuel is low");
+        } else {
+            System.out.println("Your car is started");
+        }
+        return this;
+    }
+
+    public float addFuel(float currentFuelOfCar){
+        this.currentFuelOfCar+=currentFuelOfCar;
+        return currentFuelOfCar;
+    }
+
+    public void drive(){
+        if (currentFuelOfCar==0){
+            System.out.println("your engine not started because you not have fuel");
+        } else {
+            start();
+            System.out.println("Car is Driving");
+            currentFuelOfCar--;
+        }
+    }
+}
+
+public class ThisKeyword {
+    public static void main(String[] args) {
+        myCar audiQ8 =  new myCar();
+        audiQ8.addFuel(7);
+        audiQ8.start().drive();
+    }
+
+}
